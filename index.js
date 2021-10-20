b674ff46ba327f665640dc73837100c13f2c7328
Edouard-Germain <edangermain@gmail.com>
2021-10-20 10:05:45 +0200

    GOT AJAX BUG FIX

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ $(function(){
         
     })
 
-    function display(){
-        characters.forEach(function(i){
-            $(".container").html(".container") +
-                $(".container").html(`<div class="CharaGot">
-                 <img src="${characters.imageUrl}" class="imgCharaGot">
-                 <h4 class="TitleCharaGot">${characters.fullName}</h4>
-                <p class="TextCharaGot">${characters.title}</p> </div>`)
+    function display(characters){
+        characters.forEach(function(character){
+            $("#CharaContain").html($("#CharaContain").html()+`
+              <div class="CharaGot col-4" >
+                 <img src="${character.imageUrl}" class="imgCharaGot">
+                 <h4 class="TitleCharaGot">${character.fullName}</h4>
+                <p class="TextCharaGot">${character.title}</p> </div>`)
             })
         }
-      
-        $('.button').click(function(e) {
-            e.preventDefault()
-            var filter = characters.filter(function(i){
-                return  characters.fullName === /// formulaire     
-        })
+    //   var  input = 
+    //     $('.button').click(function(e) {
+    //         e.preventDefault()
+    //         var filter = characters.filter(function(i){
+    //             return  characters.fullName ===      
+    //     })
    
 })
